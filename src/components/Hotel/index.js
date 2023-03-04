504e563ae43c106eaea592f4f684419e66f9e045
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-04 10:56:00 -0300

    feat: Update HotelPage component to handleRooms onClick

--- src/components/Hotel/index.js
+++ src/components/Hotel/index.js
@@ -1,19 +1,22 @@
 import * as S from './style';
 
-export default function HotelPage({ hotel, selected, setSelect }) {
-  const handleSelected = (id) => {
-    selected === id ? setSelect(0) : setSelect(id);
-  };
-
-  return (
-    <S.HotelWrapper key={hotel.id} clicked={selected === hotel.id} onClick={() => handleSelected(hotel.id)}>
+export default function HotelPage({ hotel, selected, handleRooms  }) {
+  return (  
+    <S.HotelWrapper key={hotel.id} clicked={selected === hotel.id} onClick={() => handleRooms(hotel.id)}>
       <img src={hotel.image} alt={hotel.name} />
-      <S.HotelName>{hotel.name}</S.HotelName>
-      <S.Subtitle>Tipos de acomodação:</S.Subtitle>
-      <S.Detail>{RoomTypes[hotel.capacity]}</S.Detail>
-      <S.Subtitle>Vagas disponíveis:</S.Subtitle>
-      <S.Detail>{hotel.availableRooms}</S.Detail>
-    </S.HotelWrapper>);
+      <>
+        <S.HotelName>{hotel.name}</S.HotelName>
+        <>
+          <S.Subtitle>Tipos de acomodação:</S.Subtitle>
+          <S.Detail>{RoomTypes[hotel.capacity]}</S.Detail>
+        </>
+        <>
+          <S.Subtitle>Vagas disponíveis:</S.Subtitle>
+          <S.Detail>{hotel.availableRooms}</S.Detail>
+        </>     
+      </>
+    </S.HotelWrapper>
+  );
 }
 
 const RoomTypes = {
