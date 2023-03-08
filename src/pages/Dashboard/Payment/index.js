28c04c5ee4f7bdfc9b77ffd5307267d05139686b
daniel-bernardino747 <dn.danielbernardino@gmail.com>
2023-03-08 12:23:24 -0300

    style(Payment): remove comment

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,30 +1,3 @@
-/*
-import TicketTypeSelection from '../../../components/TicketTypeSelection';
-import * as S from '../../../components/Typography';
-import PageUnavailable from '../../../components/Page/Unavailable';
-import useCheckEnrollment from '../../../hooks/api/useTicket';
-import PaymentSelection from '../../../components/Payment/index';
-
-export default function Payment() {
-  const { userEnrollment } = useCheckEnrollment();
-
-  if (!userEnrollment)
-    return (
-      <>
-        <S.StyledTypography variant="h4">Ingresso e pagamento</S.StyledTypography>
-        <PageUnavailable>
-          <S.StyledTypography variant="h6">
-            Você precisa completar sua inscrição antes de prosseguir pra escolha de ingresso
-          </S.StyledTypography>
-        </PageUnavailable>
-      </>
-    );
-
-  return <TicketTypeSelection />;
-  //return <PaymentSelection />;
-}
-*/
-
 import Payment from '../../Payment/index';
 
 export default function FillSubscription() {
