7db95130885f3fc45de0645c8a38dd3755897c3f
mcrestani <marianascrestani@gmail.com>
2023-03-09 09:45:18 -0300

    feat: clear BookingPage

--- src/components/Hotel/Booking.js
+++ src/components/Hotel/Booking.js
@@ -18,24 +18,3 @@ export function BookingPage() {
     </S.HotelWrapper>
   );
 }
-
-/*
- return (
-    <S.HotelWrapper key={hotel.id}>
-      <img src={hotel.image} alt={hotel.name} />
-      <>
-        <S.HotelName>{hotel.name}</S.HotelName>
-        <>
-          <S.Subtitle>Quarto reservado</S.Subtitle>
-          <S.Detail>{RoomTypes[hotel.capacity]}</S.Detail>
-        </>
-        <>
-          <S.Subtitle>Pessoas no seu quarto</S.Subtitle>
-          <S.Detail>{hotel.availableRooms}</S.Detail>
-        </>
-      </>
-    </S.HotelWrapper>
-  );
-}
-
-*/
